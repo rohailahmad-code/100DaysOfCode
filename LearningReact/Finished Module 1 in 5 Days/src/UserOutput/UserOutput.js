78c19efb86fcecff479f4bbf22bbcad000e08444
Rohail ahmad <70832936+rohailahmad-code@users.noreply.github.com>
2022-03-26 13:45:01 +0500

    Add files via upload

---
+++ LearningReact/Finished Module 1 in 5 Days/src/UserOutput/UserOutput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const userOutput = () => {
+    return  (
+        <div>
+            <p>Some Random Text</p>
+            <p>I hope I will be Overwirtten</p>
+        </div>
+    )
+}
